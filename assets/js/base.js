987571f55477089a9ed4b1b27d179fefa0a7d09c
hamlim <hamlim@rpi.edu>
2016-01-23 12:24:24 -0800

    adding a thanks page

---
+++ assets/js/base.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     //on button click we want to submit the contact form
+//     $('#contact-submit').on('click', function(){
+//         //we want to get all the form content
+//         var emailelem = $('#contactEmailInput');
+//         var nameelem = $('#contactNameInput');
+//         var messageelem = $('.contactMessage');
+//         if(messageelem.value != "" && nameelem.value != "" && emailelem != ""){
+//             notie.alert(3, "Make sure to fill every field!", 5);
+//         } else {
+//             var message = messageelem.value;
+//             var fromEmail = emailelem.value;
+//             var fromName = nameelem.value;
+//
+//         }
+//     });
+// });
