0250cbf6a014e7552c6a907bccdd45dc7aa12d96
Matt Hamlin <hamlim@outlook.com>
2016-09-26 19:02:08 -0400

    editing the base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,17 +1,12 @@
-// $(document).ready(function(){
-//     //on button click we want to submit the contact form
-//     $('#contact-submit').on('click', function(){
-//         //we want to get all the form content
-//         var emailelem = $('#contactEmailInput');
-//         var nameelem = $('#contactNameInput');
-//         var messageelem = $('.contactMessage');
-//         if(messageelem.value != "" && nameelem.value != "" && emailelem != ""){
-//             notie.alert(3, "Make sure to fill every field!", 5);
-//         } else {
-//             var message = messageelem.value;
-//             var fromEmail = emailelem.value;
-//             var fromName = nameelem.value;
-//
-//         }
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var location = {};
+	getlocation = new XMLHttpRequest();
+	getlocation.onreadystatechange = function(){ 
+		console.log(getlocation.responseText);
+	}
+	getlocation.open('GET', 'https://geoip-db.com/json/');
+	getlocation.send();
+
+	var package = {};
+	package.location = JSON.stringify()
+});
