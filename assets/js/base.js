1b244c8714357779bfb8e17df64300ec8c219b11
Matt Hamlin <hamlim@outlook.com>
2019-01-01 12:34:28 -0800

    More https assets than http

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-	var userlocation;
-	getlocation = new XMLHttpRequest();
-	getlocation.onreadystatechange = function(){ 
-		userlocation = getlocation.responseText;
-	}
-	getlocation.open('GET', '//freegeoip.net/json/', 'false'); //ugh just because I don't want to spin up some Promises
-	getlocation.send();
+document.addEventListener('DOMContentLoaded', function(event) {
+  var userlocation
+  getlocation = new XMLHttpRequest()
+  getlocation.onreadystatechange = function() {
+    userlocation = getlocation.responseText
+  }
+  getlocation.open('GET', '//freegeoip.net/json/', 'false') //ugh just because I don't want to spin up some Promises
+  getlocation.send()
 
-	var package = {};
-	package.message = JSON.stringify(userlocation);
-	sendlocation = new XMLHttpRequest();
-	sendlocation.open('POST', 'http://formspree.io/neil@kymainternational.com');
-	sendlocation.setRequestHeader('Accept', 'application/json');
-	sendlocation.send(JSON.stringify(package));
-});
+  var package = {}
+  package.message = JSON.stringify(userlocation)
+  sendlocation = new XMLHttpRequest()
+  sendlocation.open('POST', 'https://formspree.io/neil@kymainternational.com')
+  sendlocation.setRequestHeader('Accept', 'application/json')
+  sendlocation.send(JSON.stringify(package))
+})
